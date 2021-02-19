f8321cc89c08e116ebffecf541d9df766ca51fe0
Punith Sharma <psharma@crestron.com>
2021-02-19 18:56:29 +0530

    XIOCLD-9629

--- lib/GitUtil.py
+++ lib/GitUtil.py
@@ -52,7 +52,7 @@ def git_push_oper(repo_path, jira_ticket):
         git_repo = git.Repo(repo_path)
         git_status = (git_repo.git.status())
         #re_add_files = '((?:[A-Za-z_]+\/){1,10}[A-Za-z]+(?:(?:.json)|(?:.xml)))'
-        re_add_files = '((?:[A-Za-z_-]+\/){1,10}[A-Za-z]+(?:(?:.json)|(?:.py)))'
+        re_add_files = '((?:[a-zA-Z0-9_-]+\/){1,10}[a-zA-Z0-9]+(?:(?:.json)|(?:.py)))'
         git_add_files = re.findall(re_add_files, git_status)
         print("Files to add it to git are %s" % git_add_files)
         for files in git_add_files:
--- testcase/AddLevel1SupportDeviceModelType.py
+++ testcase/AddLevel1SupportDeviceModelType.py
@@ -2,6 +2,7 @@ import lib.CommonUtil as utility
 import lib.GitUtil as git
 import variableFile
 import json
+import time
 
 
 app_set_name = variableFile.app_setting_name
@@ -19,6 +20,7 @@ prd_e = variableFile.env_value_prd_e
 prd_w = variableFile.env_value_prd_w
 app_set_file_path = variableFile.app_set_file_path
 device_model_type_file = variableFile.device_model_type_file
+device_view_model_file = variableFile.device_view_model_file
 device_model_type = variableFile.device_model_type
 git_branch = variableFile.git_branch
 git_branch_re = variableFile.git_branch_re
@@ -29,12 +31,11 @@ jira_ticket = variableFile.jira_ticket
 
 class AddL1SupportDeviceModelType:
     def __init__(self):
-        print("Adding level 1 support for new device models")
+        print("Addressing the Level 1 support for Device Model Type and Device View Model ticket %s" % jira_ticket)
 
     def sync_local_to_remote_repo(self):
         global repo_path, git_branch, git_branch_re, git_pull_success_status
         try:
-            print("This is a 'sync_local_to_remote_repo' method")
             self.flag = False
             git_pull_status = git.git_pull_oper(repo_path=repo_path, git_branch=git_branch,
                                                 git_branch_re=git_branch_re,
@@ -51,15 +52,53 @@ class AddL1SupportDeviceModelType:
 
     def add_device_model_type_file(self):
         try:
+            self.model_type_flag = False
             if self.flag:
-                self.model_type_flag = False
                 with open(device_model_type_file) as model_type_file:
                     data = json.load(model_type_file)
                     data['id'] = device_model_type
                     data['modelType'] = device_model_type
+                    print("Data for model type file is created successfully")
                     self.model_type_flag = True
 
                 if self.model_type_flag:
                     with open('devicemodeltypes_%s.json' % device_model_type, 'w') as outfile:
-                        json.dump(data, outfile)
+                        json.dump(obj=data, fp=outfile, indent=2)
+                        outfile.close()
+                        time.sleep(2)
+                        print("Creation of Device Model Type file is successful")
+                else:
+                    print("Creation of Device Model Type file is failed")
+            else:
+                print("Git repo is out of sync, please do fix it")
+        except Exception as err:
+            print("Adding Device Model Type file failed with exception %s" % err)
 
+    def add_device_view_model_file(self):
+        try:
+            self.view_model_flag = False
+            if self.flag:
+                with open(device_view_model_file) as view_model_file:
+                    data = json.load(view_model_file)
+                    data['id'] = device_model_type
+                    print("Data for model type file is created successfully")
+                    self.view_model_flag = True
+
+                if self.view_model_flag:
+                    with open('deviceviewmodels_%s.json' % device_model_type, 'w') as outfile:
+                        json.dump(obj=data, fp=outfile, indent=2)
+                        outfile.close()
+                        time.sleep(2)
+                        print("Creation of Device View Model file is successful")
+                else:
+                    print("Creation of Device View Model file is failed")
+            else:
+                print("Git repo is out of sync, please do fix it")
+        except Exception as err:
+            print("Adding Device View Model file failed with exception %s" % err)
+
+
+level1_support = AddL1SupportDeviceModelType()
+level1_support.sync_local_to_remote_repo()
+level1_support.add_device_model_type_file()
+level1_support.add_device_view_model_file()
