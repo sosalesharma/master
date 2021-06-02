255a89cf2ff00a3804762aad1ac1e738def76d45
Punith Sharma <psharma@crestron.com>
2021-06-02 15:50:10 +0530

    XIOCLD-11651

--- lib/GitUtil.py
+++ lib/GitUtil.py
@@ -1,12 +1,5 @@
-import json
-import sys
-import logging as logger
 import git
 import re
-import lib.CommonUtil as utility
-from git import Repo
-from git import RemoteProgress
-
 
 
 def verify_git_branch(repo_path, git_branch, git_branch_re):
@@ -55,6 +48,7 @@ def git_push_oper(repo_path, jira_ticket):
         #re_add_files = '((?:[A-Za-z_]+\/){1,10}[A-Za-z]+(?:(?:.json)|(?:.xml)))'
         # (?:[a-zA-Z0-9_-]+\/){1,10}[a-zA-Z0-9_-]+(?:\.json)|(?:\.py)
         re_add_files = '((?:[a-zA-Z0-9_-]+\/){1,10}[a-zA-Z0-9_-]+(?:(?:\.json)|(?:\.py)))'
+        #re_add_files = '((?:[a-zA-Z0-9_-]+\/){1,10}[a-zA-Z0-9_-]+(?:(?:\.json)|(?:\.xml)))'
         git_add_files = re.findall(re_add_files, git_status)
         print("Files to add it to git are %s" % git_add_files)
         for files in git_add_files:
--- testcase/variableFile.py
+++ testcase/variableFile.py
@@ -1,5 +1,15 @@
+# Common variables
+repo_path = 'C:\\Users\\psharma\\PycharmProjects\\XioCloud'
+#repo_path = 'C:\\XIOCLOUD'
+git_branch = 'master'
+git_branch_re = "'origin\/(.*)'"
+git_pull_success_status = 'up to date'
+git_cw_dir = 'C:\\XIOCLOUD'
+jira_ticket = 'XIOCLD-11651'
+
+# AppSetting variables
 app_setting_name = "AccountActivationLink"
-app_api_name = "-zuoraaccountsvc-api123"
+app_api_name = "-zuoraaccountsvc-api12"
 env_value_all = "3"
 env_value_dv2 = "10"
 env_value_dv3 = None
@@ -11,15 +21,9 @@ env_value_pre_w = None
 env_value_prd_e = None
 env_value_prd_w = None
 env_value = None # value should be 'dynamic' inorder to get the id of the app service should be fetched from the az portal
-jira_ticket = 'XIOCLD-9629'
 app_set_file_path = 'C:\\Users\\psharma\\PycharmProjects\\XioCloud\\testcase\\AppSetting.json'
+
+# Level 1 support variables
 template_dir = 'C:\\Users\\psharma\\PycharmProjects\\XioCloud\\lib\\templates\\'
-device_view_model_file = 'C:\\Users\\psharma\\PycharmProjects\\XioCloud\\lib\\templates\\deviceviewmodel.json'
 level1_abs_path = 'C:\\Users\\psharma\\PycharmProjects\\XioCloud\\testcase\\'
-device_model_type = 'PC-350V-12'
-git_branch = 'master'
-git_branch_re = "'origin\/(.*)'"
-git_pull_success_status = 'up to date'
-git_cw_dir = 'C:\\Users\\psharma\\PycharmProjects\\XioCloud'
-#repo_path = 'C:\\Users\\psharma\\Source\\Repos\\CrestronEngineering\\XIOCLOUD'
-repo_path = 'C:\\Users\\psharma\\PycharmProjects\\XioCloud'
+device_model_type = 'HD-PS622'
